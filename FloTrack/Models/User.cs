1834cd90a208ad53673cf43028f907715e2eb498
Flo <flo.hartl@gmx.net>
2025-01-06 17:49:23 +0100

    User Model

---
+++ FloTrack/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace FloTrack.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public decimal Weight { get; set; }  
+        public decimal Height { get; set; }  
+        public decimal BodyFatPercentage { get; set; } 
+        public decimal ActivityLevel { get; set; }
+        public int GoalCalories { get; set; }
+    }
+
+}
